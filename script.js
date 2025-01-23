ba844155334b342311c80d64442641e841dc753d
Azlangoat <azlan.ahmed@student.tdsb.on.ca>
2025-01-23 11:11:30 -0500

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@
 // Model URL from Teachable Machine
 //**************************************************
 //* as before, paste your lnk below
-let URL = "https://teachablemachine.withgoogle.com/models/JqxMq9nEr/";
+let URL = "https://teachablemachine.withgoogle.com/models/IwWookARf/";
 
 
 
@@ -231,7 +231,7 @@ async function playInstructionVideo() {
     if (model) {
         processFrame();
     } else {
-        console.log("https://teachablemachine.withgoogle.com/models/JqxMq9nEr/");
+        console.log("https://teachablemachine.withgoogle.com/models/IwWookARf/");
     }
 }
 
